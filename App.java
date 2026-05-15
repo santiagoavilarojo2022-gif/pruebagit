864cf28007916b1b92a36b87e686f0f78342d56d
santiagoavila2006git <santiagoavilarojo2022@gmail.com>
2026-05-14 23:40:34 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,15 +1,14 @@
 public class App {
     public static void main(String[] args) {
-        // Instanciamos la universidad (que ya contiene el índice hash)
         universidad uni = new universidad();
 
         System.out.println("--- REGISTRANDO ESTUDIANTES ---");
-        // Nota: Le agregamos el legajo al final de cada constructor
+        // Le agrego el legajo al final de cada constructor
         clase_estudiante e1 = new clase_estudiante("Ana", "Gomez", 20, "111", "Sistemas", 8.5f, "Matemáticas", "AB12");
         clase_estudiante e2 = new clase_estudiante("Luis", "Perez", 21, "222", "Sistemas", 7.0f, "Física", "CD34");
         clase_estudiante e3 = new clase_estudiante("Marta", "Ruiz", 22, "333", "Sistemas", 9.0f, "Química", "EF56");
         
-        // Vamos a forzar una colisión si sus sumas ASCII caen en la misma posición base
+        // Voy a forzar una colisión
         clase_estudiante e4 = new clase_estudiante("Pedro", "Diaz", 23, "444", "Sistemas", 6.0f, "Redes", "AB12"); // Mismo legajo para forzar colisión/exploración
 
         uni.registrarEstudiante(e1);
